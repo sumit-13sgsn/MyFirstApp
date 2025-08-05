64ec05ff7f6b93ed286ca0e9ac6c6179652aa0ad
Sumit Rana <sumit.rana@sgsnassociates.com>
2025-08-05 12:08:20 +0530

    main branch fix

--- obj/Debug/net9.0/MyFirstApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5bdf57253f5bd3df200089d86b7c5af19707366")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da39b84893207f532dfe7c96d161ce39c86d37b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
